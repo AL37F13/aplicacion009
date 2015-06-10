0b1ee9a749aad9728c70141917ef1aee66c79be6
AL37F13 <lucero.rc.16@gmail.com>
2015-06-10 13:18:14 -0500

    cinco
    cambios en index y acciones

--- js/acciones.js
+++ js/acciones.js
@@ -1,6 +1,20 @@
 // JavaScript Document
 $(document).ready(function(e) {
-document.addEventListener("deviceready",function(){
+document.addEventListener("deviceready",function(){ 
+var basedatos=window.sqlitePlugin.openDatabase({name: "coloresBD.db",createFromLocation:1});
+cargarnombrejugador ();
+
+
+function cargarnombrejugador ()
+{
+	basedatos.transaction(function(ejecutar){
+		var sql="SELECT NombreUsuario FROM Usuario";
+		ejecutar.executeSql(sql,undefined,function(ejecutar,resultado){
+		var datosJugador =resultado.rows.item(0);
+		$('#jugador').text(datosJugador.NombreUsuario);
+		});
+	});
+}
 	
 	audio=window.plugins.LowLatencyAudio;
 	audio.preloadFX('B1', 'audio/C.mp3',function(){},function(msg){alert("error "+msg);});
@@ -16,19 +30,36 @@ document.addEventListener("deviceready",function(){
 		var alto=(pantalla-encabezado-pie)/2;
 		$('.cuadro').height( alto);	
 	});
-	$('.cuadro').on('vmousedown',function (){
-		$('#pantalla').append(quien($(this).attr('id')));
-		$(this).addClass('pulsado');
-	});
-	$('.cuadro').on('vmouseup',function (){
-		$(this).removeClass('pulsado');
-	});
+	//$('.cuadro').on('vmousedown',function (){
+		//$('#pantalla').append(quien($(this).attr('id')));
+		//$(this).addClass('pulsado');
+	//});
+	//$('.cuadro').on('vmouseup',function (){
+		//$(this).removeClass('pulsado');
+	//});
 	function quien (q)
 	{
 		audio.play(q);
 		return q.substring(1);
 	}
-	
+	$('#btnconfigurar').on('tap',function(){
+		$('#txtnombre').val($('#jugador').text());
+	});
+	$('#btnguardar').on('tap',function (){
+		var nuevonombre=$('#txtnombre').val();
+		basedatos.transaction(function( consulta){
+		consulta.executeSql("UPDATE Usuario SET NombreUsuario =? WHERE ClaveUsuario='1';",[nuevonombre]);
+		});
+		cargarnombrejugador()
+	function flash (boton)
+	{
+	boton.stop().animate({opacity:'0.5'},{
+		duration:80, complete:function(){
+		boton.stop().animate({opacity:'1'},200);
+	}
+	});
+	}
+	});
 }); 
 });
 
